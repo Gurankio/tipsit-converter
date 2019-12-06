2107deb2a2f82aec5b3b4241490ca3696a8ac928
Raptor803 <55805414+Raptor803@users.noreply.github.com>
2019-12-06 15:44:13 +0100

    Added "toRoman" and "addNchar" functions

--- nbcl.h
+++ nbcl.h
@@ -11,11 +11,22 @@
 using namespace std;
 
 // double (base 10) --> string (base n)
-string toBase(double, int, int);
+string toBase(string, int, int);
 
 // string (base N) --> double (base 10)
 double fromBase(string, int);
 
+// int (base 10) --> string (roman) !up to 3999
+string toRoman(int n);
+
 // utils
 int getValue(char);
 string reverse(string);
+string addNchar(string, char, int); 
+/* adds N chars to a string
+ ex. 
+ string s = "";
+ s = addNchar(s, 'Y', 3);
+ cout << s << endl;
+ // output = "YYY"
+*/
